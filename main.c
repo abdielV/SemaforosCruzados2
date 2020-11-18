6e6ef42c302ba394bf891569fc53d76c4a70a983
AbdielV <abdielvitalini@gmail.com>
2020-11-17 19:36:58 -0600

    Se cambiaron los productores y consumidores para que cada uno tuviera 3 y se agregó un buffer, para tener un total de 2.

--- main.c
+++ main.c
@@ -5,24 +5,33 @@
 #include<string.h>
 #include<pthread.h>
 
-const int repeticiones = 20;
-char buffer[10];
+const int repeticiones = 30;
+char buffer1[10];
+char buffer2[10];
 
 void * productor(void * parametro);
 void * consumidor(void * parametro);
 
 int main(void){
-    pthread_t prod1, prod2, cons;
-    sem_open("productor", O_CREAT, S_IRUSR | S_IWUSR, 1);//Lectura y escritura para el usuario
-    sem_open("consumidor", O_CREAT, S_IRUSR | S_IWUSR, 0);//S_IRUSR y S_IWUSR son parte de fcntl.h
-    
-    pthread_create(&prod1, NULL, productor, "1234");
-    pthread_create(&prod2, NULL, productor, "5678");
-    pthread_create(&cons, NULL, consumidor, NULL);
+    pthread_t prod1, prod2, prod3, cons1, cons2, cons3;
+    sem_open("productor1", O_CREAT, S_IRUSR | S_IWUSR, 1);//Lectura y escritura para el usuario
+    sem_open("consumidor1", O_CREAT, S_IRUSR | S_IWUSR, 0);//S_IRUSR y S_IWUSR son parte de fcntl.h
+    sem_open("productor2", O_CREAT, S_IRUSR | S_IWUSR, 1);
+    sem_open("consumidor2", O_CREAT, S_IRUSR | S_IWUSR, 0);
+
+    pthread_create(&prod1, NULL, productor, "0--");
+    pthread_create(&prod2, NULL, productor, "-1-");
+    pthread_create(&prod3, NULL, productor, "--2");
+    pthread_create(&cons1, NULL, consumidor, NULL);
+    pthread_create(&cons2, NULL, consumidor, NULL);
+    pthread_create(&cons3, NULL, consumidor, NULL);
 
     pthread_join(prod1, NULL);
     pthread_join(prod2, NULL);
-    pthread_join(cons, NULL);
+    pthread_join(prod3, NULL);
+    pthread_join(cons1, NULL);
+    pthread_join(cons2, NULL);
+    pthread_join(cons3, NULL);
     
     return 0;
 }
@@ -34,14 +43,23 @@ void * productor(void * parametro){
     else
         strcpy(cadena, (char *)parametro);
     
-
-    sem_t * semProd = sem_open("productor", 0);
-    sem_t * semCons = sem_open("consumidor", 0);
+    sem_t * semProd;
+    sem_t * semCons;
+    char * buffer;
 
     for(int i = 0; i < repeticiones; i++){
+        if(i%2){
+            semProd = sem_open("productor1", 0);
+            semCons = sem_open("consumidor1", 0);
+            buffer = buffer1;
+        } else {
+            semProd = sem_open("productor2", 0);
+            semCons = sem_open("consumidor2", 0);
+            buffer = buffer2;
+        }
         sem_wait(semProd);
-        printf("Productor en la iteración(%d)\n", i);fflush(stdout);
         strcpy(buffer, cadena);
+        printf("P(%s):%d\n", buffer, i);fflush(stdout);
         sem_post(semCons);
     }
 
@@ -49,12 +67,22 @@ void * productor(void * parametro){
 }
 
 void * consumidor(void * parametro){
-    sem_t * semProd = sem_open("productor", 0);
-    sem_t * semCons = sem_open("consumidor", 0);
+    sem_t * semProd;
+    sem_t * semCons;
+    char * buffer;
     
-    for(int i = 0; i < repeticiones * 2; i++){
+    for(int i = 0; i < repeticiones; i++){
+        if(i%2){
+            semProd = sem_open("productor1", 0);
+            semCons = sem_open("consumidor1", 0);
+            buffer = buffer1;
+        } else {
+            semProd = sem_open("productor2", 0);
+            semCons = sem_open("consumidor2", 0);
+            buffer = buffer2;
+        }
         sem_wait(semCons);
-        printf("\t\t\t\t...Consumido___%s___\n", buffer);fflush(stdout);
+        printf("C(%s):%d\n", buffer, i);fflush(stdout);
         sem_post(semProd);
     }
 
